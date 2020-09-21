30d21dc04f71c1496eb816300ceb294cbbba6351
inada <starmelt190@gmail.com>
2020-09-21 11:52:31 +0900

    お問い合わせ項目編集

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -3,10 +3,12 @@
     <h1 class="contact-title"><font-awesome-icon icon="mail-bulk" style="padding-right: 10px" />Contact</h1>
       <div class="contact">
         <div class="contact-description">
-        現在求職中の身となります、エンジニアとしてマルチに活躍できる様フレームワークよりRailsで作成してみたり、Vueを使用したりなどしております。お仕事に関し現在は企業に在籍したいと考えております。
+        現在、エンジニアとしてお仕事に関し携われる企業に在籍したいと考えており連絡先は下記となります。
+        是非、お話の機会を頂けますと幸いでございます。
         
-        私宛の連絡先はこちらのアドレスとなります。
-        email：
+        【email】
+        t.inada_0511@gmail.com
+        <a href="https://www.wantedly.com/users/137652213">【Wantedly経由でのお問い合わせはこちら】</a>
         </div>
       </div>
   </div>
